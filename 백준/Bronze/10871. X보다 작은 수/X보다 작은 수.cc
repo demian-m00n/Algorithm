670633715477ff96cb4450091eb04c64590a4e56
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-07-18 21:48:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n , x , a[10000];
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> x;
+  for(int i = 0 ; i < n ; i++) cin >> a[i];
+  for(int i = 0 ; i < n ; i++)
+    if(a[i] < x) cout << a[i] << ' ';
+}
