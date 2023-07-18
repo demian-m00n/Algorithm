2951aae68d4047ae4b1c03bf53c7334b90ff4312
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-07-18 22:09:04 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a,b;
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> a >> b;
+  cout << a+b << '\n' << a-b << '\n' << a*b <<'\n';
+  cout << a/b << '\n' << a%b;
+}
