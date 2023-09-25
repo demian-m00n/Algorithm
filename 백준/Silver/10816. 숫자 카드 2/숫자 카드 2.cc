b788f1a3a3be565afad4a4ea5f985d7ccb49142b
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-09-25 16:58:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 344 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[1000005];
+int n;
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  cin >> n;
+  for(int i=0;i<n;++i) cin >> a[i];
+  sort(a,a+n);
+  int m;
+  cin >> m;
+  while(m--){
+    int t;
+    cin >> t;
+    cout << upper_bound(a,a+n,t) - lower_bound(a,a+n,t) << ' ';
+  }
+}
