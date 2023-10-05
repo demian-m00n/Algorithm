8cc06ba2d2dd4bda6c1ea06a6e98fc35744f2356
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-10-05 14:53:10 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  int n ;
+  cin >> n ;
+  for(int i = 1 ; i <= n ; ++i){
+
+    for(int k = n - i ; k > 0 ; --k) cout << ' ' ;
+    for(int j = 0 ; j < 2 * i - 1 ; ++j ) cout << '*' ;
+
+    cout << '\n' ;
+  }
+}
