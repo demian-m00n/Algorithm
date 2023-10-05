684a6f979b6dd0ce19e268f2b7544ba4422be248
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-10-05 15:02:39 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  int n ;
+  cin >> n ;
+
+  for(int i = n ; i > 0 ; --i){
+
+    for(int k = n - i ; k > 0 ; --k) cout << ' ' ;
+    for(int j = 0 ; j < 2 * i - 1 ; ++j ) cout << '*' ;
+
+    cout << '\n' ;
+  }
+
+  for(int i = 2 ; i <= n ; ++i){
+
+    for(int k = n - i ; k > 0 ; --k) cout << ' ' ;
+    for(int j = 0 ; j < 2 * i - 1 ; ++j ) cout << '*' ;
+
+    cout << '\n' ;
+  }
+
+}
