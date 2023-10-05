41f56eeb2fc47d4170ff5f41b0311c5549359a2d
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-10-05 12:47:19 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  int arr[5];
+  for(int i = 0 ; i < 5 ; ++i) cin >> arr[i] ;
+  int mean = 0 ;
+  for(int x : arr) mean += x ;
+  mean /= 5 ;
+  sort(arr,arr+5) ;
+  cout << mean << '\n' << arr[2] ;
+}
