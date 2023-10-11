9d12390d3f162fdc9fccacf09460f3e4cb25f066
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-10-11 15:44:14 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool isUsed[10];
+int num[10];
+
+void func(int k){
+  if(k == m){
+    for(int i = 0 ; i < m ; ++i)
+      cout << num[i] << ' ';
+    cout << '\n';
+    return ;
+  }
+  int tmp = 0;
+  for(int i = 0 ; i < n ; ++i){
+    if(isUsed[i]) continue;
+    if(tmp == arr[i]) continue;
+    num[k] = arr[i];
+    tmp = num[k];
+    isUsed[i] = true;
+    func(k+1);
+    isUsed[i] = false;
+  }
+}
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  cin >> n >> m;
+  for(int i = 0 ; i < n ; ++i)
+    cin >> arr[i];
+  sort(arr,arr+n);
+  func(0);
+}
