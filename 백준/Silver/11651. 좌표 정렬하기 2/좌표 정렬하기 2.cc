7e9303216bbee5fa6e2f783fdb92f71a59c7fe54
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-11-21 18:28:33 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 52 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  int n;
+  cin >> n;
+  pair<int,int> x[n];
+  for(auto &s : x) cin >> s.first >> s.second;
+
+  sort(x, x+n, [&](pair<int,int> a, pair<int,int> b){
+    if(a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+  });
+
+  for(auto s : x){
+    cout << s.first << ' ' << s.second << '\n';
+  }
+}
