254430dd53bb966c398f189a7a9d3a7812148972
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-11-23 21:39:23 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5972 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string str1, str2;
+int d[1005][1005];
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  cin >> str1 >> str2;
+
+  int len1 = str1.length();
+  int len2 = str2.length();
+
+  for(int i = 1 ; i <= len1 ; ++i){
+    for(int j = 1 ; j <= len2 ; ++j){
+      if(str1[i-1] == str2[j-1]) d[i][j] = d[i-1][j-1] + 1;
+      else d[i][j] = max(d[i-1][j], d[i][j-1]);
+    }
+  }
+
+  cout << d[len1][len2];
+}
