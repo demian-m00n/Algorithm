ab0300b361c2ba15c8c5a801c6c3b11190926ba8
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2024-04-16 19:37:51 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<bool> a(10,0);
+int n;
+
+int main(void){
+  ios::sync_with_stdio(0),cin.tie(0);
+  cin >> n;
+
+  int cnt = 0;
+
+  for(int i = 1 ; i <= 10; ++i){
+    a[10-i] = 1;
+    do {
+      if(cnt != n) {
+        cnt++;
+        continue;
+      }
+      for(int i = 0; i < 10; i++)
+        if(a[i]) cout << 9 - i;
+      return 0;
+    } while(next_permutation(a.begin(), a.end()));
+  }
+  cout << -1;
+}
