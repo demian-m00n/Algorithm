bc0530f3a39d743eb051347cb3234d2925f277ee
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2024-06-08 12:48:09 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1248 ms, Memory: 165736 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MXN = 1'000'010;
+int N, u, v;
+vector<int> adj[MXN], tree[MXN], state[MXN];
+
+void mktree(int curr, int prev) {
+  for (int nxt : adj[curr]) {
+    if (nxt == prev) continue;
+    tree[curr].push_back(nxt);
+    mktree(nxt, curr);
+  }
+}
+
+int dp(int curr, bool isEarlyAdaptor) {
+  if (state[curr][isEarlyAdaptor] != -1)
+    return state[curr][isEarlyAdaptor];
+
+  int val = 0;
+  if (isEarlyAdaptor) 
+    for (int nxt : tree[curr])
+      val += min(dp(nxt, false), dp(nxt, true));
+  else
+    for (int nxt : tree[curr]) 
+      val += dp(nxt, true);
+
+  return state[curr][isEarlyAdaptor] = val + isEarlyAdaptor;
+}
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+  for (int i = 1; i <= N; i++) {
+    state[i].push_back(-1);
+    state[i].push_back(-1);
+  }
+  for (int i = 0; i < N - 1; i++) {
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  mktree(1, -1);
+  cout << min(dp(1, false), dp(1, true));
+}
