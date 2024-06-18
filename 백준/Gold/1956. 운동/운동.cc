11a74ebeb7451b54bf4edb6b6964edc42911bc34
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2024-06-19 01:46:38 +0900

    [Gold IV] Title: 운동, Time: 88 ms, Memory: 2652 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.cc
+++ 백준/Gold/1956. 운동/운동.cc
@@ -1,37 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-const int INF = 987987987;
-
-int v, e;
-int d[402][402];
+const int INF = 300'000'000;
+int dis[402][402];
+int V, E;
 
 int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  ios::sync_with_stdio(0),cin.tie(0);
+  cin >> V >> E;
 
-  cin >> v >> e;
-  for(int i = 1; i <= v; i++) {
-    fill(d[i] + 1, d[i] + v + 1, INF);
-    d[i][i] = 0;
+  for(int i = 1 ; i <= V ; ++i){
+    fill(dis[i] + 1, dis[i] + V + 1 , INF);
+    dis[i][i] = 0;
   }
 
   int a, b, c;
-  while(e--){
+  while(E--){
     cin >> a >> b >> c;
-    d[a][b] = c;
+    dis[a][b] = c;
   }
 
-  for(int k = 1; k <= v; k++)
-    for(int i = 1; i <= v; i++)
-      for(int j = 1; j <= v; j++)
-        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+  for(int k = 1 ; k <= V ; ++k)
+    for(int i = 1 ; i <= V ; ++i)
+      for(int j = 1 ; j <= V ; ++j)
+        dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
 
-  int ans = INF;
-  for(int i = 1; i <= v; i++)
-    for(int j = i + 1; j <= v; j++)
-      ans = min(ans, d[i][j] + d[j][i]);
-        
-  if(ans == INF) ans = -1;
-  cout << ans;
+
+  int mn = INF;
+  for(int i = 1 ; i <= V ; ++i)
+    for(int j = i + 1 ; j <= V ; ++j)
+      mn = min(mn, dis[i][j] + dis[j][i]);
+
+  if(mn == INF) mn = -1;
+  
+  cout << mn;
 }
