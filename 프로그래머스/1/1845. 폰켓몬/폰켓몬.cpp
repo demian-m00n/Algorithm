bd82952e5d97dc7e88f71c516b968426e7993fb9
Seolmin Moon <130627928+demian-m00n@users.noreply.github.com>
2024-10-18 00:16:44 +0900

    [level 1] Title: 폰켓몬, Time: 0.68 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int solution(vector<int> nums)
 {
     ios::sync_with_stdio(0),cin.tie(0);
-    set<int> s;
-    for(int n : nums)
-        s.insert(n);
-    return s.size() > nums.size() / 2 ? nums.size() / 2 : s.size();
+    unordered_set<int> s(nums.begin(), nums.end());
+    return min(nums.size() / 2, s.size());
 }
